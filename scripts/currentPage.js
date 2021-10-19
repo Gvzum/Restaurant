4bb29fd260c010af880b904d6252d013395dd08a
Gvzum <diasgazim@icloud.com>
2021-10-19 10:52:47 +0600

    Finishgit add .  Yeaesdadasdas

--- scripts/currentPage.js
+++ scripts/currentPage.js
@@ -3,4 +3,14 @@ $(document).ready(() => {
     $('.food__coll').click(() => {
         $('.food__view').toggle('slow'); 
     });
+
+    $('.hamburger').click(() => {
+        $('.header__links').toggleClass('actives');
+        $('.header__login').toggleClass('actives'); 
+    }); 
+
+    $('.menu-drop').hide(); 
+    $('.main__title').click(() => {
+            $('.menu-drop').toggle('slow'); 
+    }); 
 })
